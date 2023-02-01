691e2181107e28c42f486f01777086568dce79a1
AndresLex <andrescamargocisf@gmailcom>
2023-02-01 14:46:07 -0500

    Primer cambio

--- Angular-router/src/app/app-routing.module.ts
+++ Angular-router/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+  },
+  // Para renderizar un modulo se hace de la siguiente manera
+  // cms "sistema de administracion de contenido" o se puede colocar admin
+  // se importa de forma diferente con una arrow function y se resuelve el import con un .then
+  {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
+  },
+  // Cuando en el link no se encuantra nada se representa con **
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Angular-router/src/app/website/pages/category/category.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { ProductsService } from 'src/app/services/products.service';
+
+import { Product } from '../../../models/product.model';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss']
+})
+export class CategoryComponent implements OnInit {
+  categoryId: string | null = null;
+  products: Product[] = [];
+  limit = 10;
+  offset = 0;
+  productId: string | null = null;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap
+    .pipe(
+      switchMap(params => {
+        this.categoryId = params.get('id');
+        if (this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+        return [];
+      })
+    )
+    .subscribe( data => {
+      this.products = data;
+    });
+    this.activatedRoute.queryParamMap
+    .subscribe(params => {
+      this.productId = params.get('product');
+    });
+  }
+
+  onLoadMore() {
+    if(this.categoryId){
+      this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+      .subscribe((data) => {
+        this.products = this.products.concat(data);
+        this.offset += this.limit;
+      });
+    }
+  }
+}
